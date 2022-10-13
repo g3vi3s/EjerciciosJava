3d6d63700f1981ab1b724afe018b692833246958
soporte <soporte@soportelinux>
2022-10-13 13:33:39 -0500

    Ejercicio2 terminado

--- Ejercicio2/src/main/java/com/g3vi3s/ejercicio2/Ejercicio2.java
+++ Ejercicio2/src/main/java/com/g3vi3s/ejercicio2/Ejercicio2.java
@@ -8,26 +8,41 @@ En la función del main se debe de declarar un objeto que va a ejecutar las tres
 package com.g3vi3s.ejercicio2;
 import java.util.Scanner;
 public class Ejercicio2 {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
     public static void main(String[] args) {
-        int opt = 0;
+        int opt = 0, l1 = 0, l2 = 0, l3 = 0;
+        String mayor = "";
         Scanner entrada = new Scanner(System.in);
+        addLados lado = new addLados();
         do{
-            System.out.print("Dame una opcion: \n1.- Agregar la logitud de los lados\n2.- Cual de los lados es mayor? \n3.- Es equilatero?\n4.-Salir");
+            System.out.println("Dame una opcion: \n1.- Agregar la logitud de los lados\n2.- ¿Cual de los lados es mayor? \n3.- Salir.");
             opt=entrada.nextInt();
+            switch (opt) {
+                case 1:
+                    l1 = lado.addlado();
+                    System.out.println("El primer lado es: "+l1);
+                    l2 = lado.addlado();
+                    System.out.println("El segundo lado es: "+l2);
+                    l3 = lado.addlado();
+                    System.out.println("El tercer lado es: "+l3);
+                    break;
+                case 2:
+                    if(l1!=0){
+                        mayor = lado.ladomayor(l1, l2, l3);
+                        System.out.println(mayor);
+                    }
+                    else{
+                        System.out.println("¡AVISO! Para realizar esta opcion primero debe de ingresar los valores de los lados del triangulo");
+                    }   break;
+                case 3:
+                    System.out.println("GRACIAS POR USAR ESTE MINI PROGRAMA");
+                    break;
+                default:
+                    System.out.println("ERROR DAME UNA OPCION VALIDA");
+                    break;
+            }
             
-            if(opt == 1){
-                
-            }
-            else if(opt == 2){
-            
-            }
-            else if(opt == 3){
-                
-            }
-            else{
-                System.out.println("GRACIAS POR USAR ESTE MINI PROGRAMA");
-            }
-            
-        }while(opt!=4);
+        }while(opt!=3);
     }
 }
